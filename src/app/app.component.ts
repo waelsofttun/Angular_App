91dd8f3674ffc6dfc4e031654870ffa8bd2dd496
waeltoken <waelsmtv@gmail.com>
2023-03-12 00:05:38 +0100

    Component selector styling and template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles:['h3 {color:black}']
 })
 export class AppComponent {
   title = 'My-internship';
