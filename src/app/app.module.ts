87142c8789aba8a9df829f6d7c86d81c7835413a
waeltoken <waelsmtv@gmail.com>
2023-03-11 01:39:29 +0100

    component Manipulation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { HandmadeComponent } from './handmade/handmade.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    HandmadeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/handmade/handmade.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+
+selector:'app-handmade',
+templateUrl:'./handmade.component.html'
+
+})
+export class HandmadeComponent{
+
+
+
+}
